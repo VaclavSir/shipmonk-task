d02dd6271d2843939e0c20887249a0404654334c
Václav Šír <vaclav.sir@gmail.com>
2019-11-26 15:39:05 +0100

    feat: default form values

--- src/InputForm.tsx
+++ src/InputForm.tsx
@@ -22,27 +22,28 @@ export const InputForm: React.FC<Props> = ({onSubmit}) => {
       <h2>Containers</h2>
       {containerKeys.map(key => {
         const baseName = `bins[${key}]`;
+        const isFirst = key === 0;
         return (
           <div className="containerForm" key={key}>
             <label>
               <div>Id</div>
-              <input type="text" name={`${baseName}.id`} required={true} ref={register}/>
+              <input type="text" name={`${baseName}.id`} required={true} ref={register} defaultValue={`Bin${key + 1}`}/>
             </label>
             <label>
               <div>Wdt</div>
-              <input type="number" name={`${baseName}.w`} required={true} ref={register}/>
+              <input type="number" name={`${baseName}.w`} required={true} ref={register} defaultValue={isFirst ? 10 : undefined}/>
             </label>
             <label>
               <div>Hgt</div>
-              <input type="number" name={`${baseName}.h`} required={true} ref={register}/>
+              <input type="number" name={`${baseName}.h`} required={true} ref={register} defaultValue={isFirst ? 10 : undefined}/>
             </label>
             <label>
               <div>Dpt</div>
-              <input type="number" name={`${baseName}.d`} required={true} ref={register}/>
+              <input type="number" name={`${baseName}.d`} required={true} ref={register} defaultValue={isFirst ? 10 : undefined}/>
             </label>
             <label>
               <div>Max. wgt.</div>
-              <input type="number" name={`${baseName}.max_wg`} required={true} ref={register}/>
+              <input type="number" name={`${baseName}.max_wg`} required={true} ref={register} defaultValue={isFirst ? 0 : undefined}/>
             </label>
             <div>
               <button onClick={event => {
@@ -64,35 +65,36 @@ export const InputForm: React.FC<Props> = ({onSubmit}) => {
       <h2>Items</h2>
       {itemKeys.map(key => {
         const baseName = `items[${key}]`;
+        const isFirst = key === 0;
         return (
           <div className="containerForm" key={key}>
             <label>
               <div>Id</div>
-              <input type="text" name={`${baseName}.id`} required={true} ref={register}/>
+              <input type="text" name={`${baseName}.id`} required={true} ref={register} defaultValue={`Item${key + 1}`}/>
             </label>
             <label>
               <div>Wdt</div>
-              <input type="number" name={`${baseName}.w`} required={true} ref={register}/>
+              <input type="number" name={`${baseName}.w`} required={true} ref={register} defaultValue={isFirst ? 4 : undefined}  />
             </label>
             <label>
               <div>Hgt</div>
-              <input type="number" name={`${baseName}.h`} required={true} ref={register}/>
+              <input type="number" name={`${baseName}.h`} required={true} ref={register} defaultValue={isFirst ? 5 : undefined}/>
             </label>
             <label>
               <div>Dpt</div>
-              <input type="number" name={`${baseName}.d`} required={true} ref={register}/>
+              <input type="number" name={`${baseName}.d`} required={true} ref={register} defaultValue={isFirst ? 6 : undefined}/>
             </label>
             <label>
               <div>Wgt</div>
-              <input type="number" name={`${baseName}.wg`} required={true} ref={register}/>
+              <input type="number" name={`${baseName}.wg`} required={true} ref={register} defaultValue={isFirst ? 0 : undefined}/>
             </label>
             <label>
               <div>Qty</div>
-              <input type="number" name={`${baseName}.q`} required={true} ref={register}/>
+              <input type="number" name={`${baseName}.q`} required={true} ref={register} defaultValue={isFirst ? 5 : undefined}/>
             </label>
             <label>
               <div>VR</div>
-              <input type="checkbox" name={`${baseName}.vr`} ref={register}/>
+              <input type="checkbox" name={`${baseName}.vr`} ref={register} defaultChecked={true}/>
             </label>
             <div>
               <button onClick={event => {
